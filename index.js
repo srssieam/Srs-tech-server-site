c857bded83033d8ea077026f69145a516f99d7eb
srs-sieam <srssieam@gmail.com>
2023-10-19 09:02:40 +0600

    received product data from client site and then store the data in mongoDB

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 // console.log(process.env.DB_USERNAME)
 
-
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xjpiwvy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +27,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("productDB");
+    const productCollection = database.collection("product");
+
+
+    app.post('/products', async(req, res)=>{
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
