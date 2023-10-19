78b189017312b801bc3eaf4a43cbafd998654606
srs-sieam <srssieam@gmail.com>
2023-10-19 08:23:31 +0600

    hided user name and password of mongodb

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express(cors());
 const PORT = process.env.PORT || 9000;
 
@@ -7,9 +10,10 @@ const PORT = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 
+// console.log(process.env.DB_USERNAME)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.xjpiwvy.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xjpiwvy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +33,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
